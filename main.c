0834229e12ebe7e3668d505b8d6345b210c113dd
Nguyễn Huy Hoàng <hoangnguyen578@gmail.com>
2022-02-06 01:32:28 +0700

    Update main.c
    LED Blink modify

--- main.c
+++ main.c
@@ -29,6 +29,9 @@ void SYS_Init(void)
     /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
     CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
 
+		/* Enable PF module clock */
+    CLK_EnableModuleClock(GPF_MODULE);
+	
     /* Enable UART clock */
     CLK_EnableModuleClock(UART0_MODULE);
 
@@ -58,6 +61,8 @@ void SYS_Init(void)
  */
 int main()
 {
+		uint32_t volatile u32DelayCount;
+	
     SYS_Init();
 
     /* Init UART to 115200-8n1 for print message */
@@ -66,6 +71,19 @@ int main()
     /* Connect UART to PC, and open a terminal tool to receive following message */
     printf("Hello World\n");
 
+	   /* Configure PF.3 as Output mode */
+    GPIO_SetMode(PF, BIT3, GPIO_MODE_OUTPUT);
+	
+		/* Use Pin Data Input/Output Control to pull specified I/O or get I/O pin status */
+    /* Set PF.3 output pin value is low */
+    PF3 = 0;
+		// Give ACMP some time to settle
+    for (u32DelayCount = 0; u32DelayCount < 10000; u32DelayCount++);
+		PF3 = 1;
+		printf("Led on\n");
+		for (u32DelayCount = 0; u32DelayCount < 10000; u32DelayCount++);
+		PF3 = 0;
+		printf("Led off\n");
     /* Got no where to go, just loop forever */
     while (1);
 }
